cb86b83b064f80a86889a6bd631129a627f91ecc
gemmagates1991 <gemmagates1991@gmail.com>
2021-06-17 16:40:50 -0500

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Write your JavaScript code here!
-
+ 
 window.addEventListener("load", function() {
    fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
       response.json().then( function(json) {
